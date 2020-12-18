85e7b30256927657a833e204ef3cb4c174bb28a1
Jasmin Wellbrock <Jasmin.Wellbrock@web.de>
2020-12-18 14:16:49 +0100

    initial commit

---
+++ src/sample/controller/SignupController.java
@@ -0,0 +1,53 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignupController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane signUp;
+
+    @FXML
+    private TextField signUpFirstName;
+
+    @FXML
+    private TextField signUpLastName;
+
+    @FXML
+    private TextField signUpUsername;
+
+    @FXML
+    private TextField signUpLocation;
+
+    @FXML
+    private CheckBox signUpCheckBoxMale;
+
+    @FXML
+    private CheckBox signUpCheckBoxFemale;
+
+    @FXML
+    private PasswordField signUpPassword;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void initialize() {
+
+        
+    }
+}
