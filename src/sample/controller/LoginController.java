66baddd676c5f5427bef88c0d43afc78fff1cb65
Jasmin Wellbrock <Jasmin.Wellbrock@web.de>
2020-12-24 15:06:33 +0100

    second commit: starting with database and connecting login and signup Screen

--- src/sample/controller/LoginController.java
+++ src/sample/controller/LoginController.java
@@ -1,10 +1,15 @@
 package sample.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,20 +31,48 @@ public class LoginController {
     private Button loginButton;
 
     @FXML
+    private Button loginSignupButton;
+
+    @FXML
     void initialize() {
 
         String loginText = loginUsername.getText().trim();
         String loginPwd = loginPassword.getText().trim();
 
+        loginSignupButton.setOnAction(event -> {
+            // Take users to signup Screen
+            loginSignupButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/view/signup.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
+
+
         loginButton.setOnAction(event -> {
             if (!loginText.equals("") || !loginPwd.equals("")) {
                 loginUser(loginText, loginPwd);
+            } else {
+                System.out.println("Error login in user");
             }
-        // 8:47
+
                 });
 
     }
 
     private void loginUser(String userName, String password ) {
+        // Check in database if the user exists, if true
+        // we take them to AddItem Screen
+
     }
 }
